aa1930493c7e45dda39a97466324f11f99b9d697
suruixin <507811581@qq.com>
2018-06-11 22:53:43 +0800

    154

--- concept/webpack.config.js
+++ concept/webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 var name = Math.random().toString(36,2).split('.')[1];
 const config = {
 	mode: 'development',		// "production" | "development" | "none"
-	entry: './app.js',   	//入口文件
+	entry: ['./app.js','./main.js'],   	//入口文件
 	output:{				//出口
 		path: path.resolve(__dirname, 'dist'),	//出口位置
-		filename: 'boundle.js'	//出口文件名
+		filename: '[chunkhash].js'	//出口文件名
 	},
 	module:{
 		rules:[{
